dc0769ba3387e9442ba2304f419cb4302119fdd0
systemcmd0122 <systemcmd02.address@gmail.com>
2024-09-19 21:58:35 +0900

    main

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { M_PLUS_1 } from "next/font/google"
 import { createClient } from "@/utils/supabase/server"
 import Navigation from "@/components/navigation/Navigation"
 import ToastProvider from "@/components/providers/ToastProvider"
+import Head from "next/head"
 
 const mPlus1 = M_PLUS_1({
   weight: ["400", "700", "900"],
@@ -12,8 +13,8 @@ const mPlus1 = M_PLUS_1({
 
 export const metadata: Metadata = {
   title: {
-    template: "Supabase認証ブログ",
-    default: "Supabase認証ブログ",
+    template: "Void Pulse",
+    default: "「Void Pulse」 は、日常の中に潜む見えないエネルギーを探求し、無限の可能性を解き放つブログです。技術やアイデア、ライフスタイルから哲学まで、多様なテーマを通じて世界の「空白」に新たな視点を提供します。深く、静かに響くパルスが、あなたの思考を刺激し、新しい発見の旅へと導きます。既成概念に囚われない、自由な視点と革新的なアイデアを共有する場所です。",
   },
 }
 
@@ -34,6 +35,23 @@ const RootLayout = async ({ children }: RootLayoutProps) => {
 
   return (
     <html lang="ja">
+      <Head>
+        {/* Favicon の設定 */}
+        <link rel="icon" href="/Void Pulse.ico" />
+
+        {/* OGP メタタグの設定 */}
+        <meta property="og:title" content="Void Pulse" />
+        <meta property="og:description" content="Supabase認証ブログ" />
+        <meta property="og:image" content="/Void-Pulse.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://example.com" />
+
+        {/* Twitter Card メタタグの設定 */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Void Pulse" />
+        <meta name="twitter:description" content="「Void Pulse」は、日常の中に潜む見えないエネルギーを探求し、無限の可能性を解き放つブログです。技術やアイデア、ライフスタイルから哲学まで、多様なテーマを通じて世界の「空白」に新たな視点を提供します。深く、静かに響くパルスが、あなたの思考を刺激し、新しい発見の旅へと導きます。既成概念に囚われない、自由な視点と革新的なアイデアを共有する場所です。" />
+        <meta name="twitter:image" content="/Void-Pulse.png" />
+      </Head>
       <body className={mPlus1.className}>
         <ToastProvider />
         <div className="flex min-h-screen flex-col">
